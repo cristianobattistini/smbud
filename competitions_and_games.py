2eec7ea11bf67fcc19ade40160c0bfd7cd0872d1
Cristiano <cristianobattistini09@gmail.com>
2024-01-04 01:20:20 +0100

    push images and code update

--- competitions_and_games.py
+++ competitions_and_games.py
@@ -1,31 +1,38 @@
 import pandas as pd
 
+currentPath ="Old_DB/"
 # Load the datasets
-competitions_df = pd.read_csv('competitions')
-games_df = pd.read_csv('games')
-game_events_df = pd.read_csv('game_events')
+competitions_df = pd.read_csv(currentPath+'competitions')
+competition_games_df = pd.read_csv(currentPath+'games')
+game_events_df = pd.read_csv(currentPath+'game_events')
 
 # Define the columns to keep for competitions and games
 competitions_columns = ["competition_id", "name", "sub_type", "type", "country_id", "country_name", "domestic_league_code", "confederation"]
 games_columns = ["game_id", "competition_id", "season", "round", "date", "home_club_id", "away_club_id", "home_club_goals", "away_club_goals", "home_club_position", "away_club_position", "home_club_manager_name", "away_club_manager_name", "stadium", "attendance", "referee", "home_club_name", "away_club_name", "aggregate"]
 
-# Raggruppa game_events_df per 'game_id' e trasforma in lista di dizionari
-events_grouped = game_events_df.groupby('game_id').apply(lambda x: x.to_dict('records')).reset_index(name='events')
+# Raggruppa game_events_df per 'game_id' e trasforma in una lista di dizionari e rimuove il game_id e dal club_id
 
-# Unisci games_df con events_grouped
-games_with_events_df = pd.merge(games_df, events_grouped, on='game_id', how='left')
+events_grouped = game_events_df.groupby('game_id').apply(lambda x: x.drop(columns=['game_id']).to_dict('records')).reset_index(name='events')
 
-# Seleziona solo le colonne desiderate per games
-games_with_events_df = games_with_events_df[games_columns]
+# Unisci club_games_df con events_grouped creando i subdocuments
+competition_games_with_events_df = pd.merge(competition_games_df, events_grouped, on='game_id', how='left')
 
-# Raggruppa games_with_events_df per 'competition_id'
-games_grouped = games_with_events_df.groupby('competition_id').apply(lambda x: x.to_dict('records')).reset_index(name='games')
-
-# Seleziona solo le colonne desiderate per competitions
+# Seleziona solo le colonne desiderate
 competitions_df = competitions_df[competitions_columns]
+competition_games_df = competition_games_df[games_columns]
 
-# Unisci competitions_df con games_grouped
-competitions_with_games_df = pd.merge(competitions_df, games_grouped, on='competition_id', how='left')
+# Raggruppa e trasforma game_events_df in una lista di dizionari, rimuovendo 'game_id'
+events_grouped = game_events_df.groupby('game_id').apply(lambda x: x.drop(columns=['game_id']).to_dict('records')).reset_index(name='events')
 
-# Esporta in JSON
-competitions_with_games_df.to_json('competitions_with_games_and_events.json', orient='records', lines=True)
+# Unisci competition_games_df con events_grouped
+competition_games_with_events_df = pd.merge(competition_games_df, events_grouped, on='game_id', how='left')
+
+# Raggruppa per 'competition_id' mantenendo 'competition_id' nei giochi
+competition_games_grouped = competition_games_with_events_df.groupby('competition_id').apply(lambda x: x.drop(columns=['competition_id']).to_dict('records')).reset_index(name='games')
+
+# Unisci competitions_df con competition_games_grouped
+competitions_with_games_df = pd.merge(competitions_df, competition_games_grouped, on='competition_id', how='left')
+
+# Esporta il DataFrame unificato in JSON
+competitions_with_games_df.to_json('DB_gen/competitions_with_games_and_events.json', orient='records', lines=True)
+
--- players_complete_info.py
+++ players_complete_info.py
@@ -4,10 +4,11 @@ import pandas as pd
 # country_of_citizenship,date_of_birth,sub_position,position,foot,height_in_cm,market_value_in_eur,highest_market_value_in_eur,
 #contract_expiration_date,agent_name,image_url,url,current_club_domestic_competition_id,current_club_name
 
+currentPath ="Old_DB/"
 # Load the datasets
-players_df = pd.read_csv('players')
-player_valuation_df = pd.read_csv('player_valuations')
-appearances_df = pd.read_csv('appearances')
+players_df = pd.read_csv(currentPath+'players')
+player_valuation_df = pd.read_csv(currentPath+'player_valuations')
+appearances_df = pd.read_csv(currentPath+'appearances')
 
 # Define the columns to keep for valuations and appearances
 valuation_columns = ['player_id', 'last_season', 'market_value_in_eur', 'current_club_id']
@@ -20,9 +21,16 @@ players_columns = ['player_id', 'first_name', 'last_name', 'name', 'last_season'
 
 # Select only the desired columns for players
 players_df = players_df[players_columns]
-# Merge valuations and appearances data into arrays within the player object
-players_df['valuations'] = players_df['player_id'].map(player_valuation_df[valuation_columns].groupby('player_id').apply(lambda x: x.to_dict('records')).to_dict())
-players_df['appearances'] = players_df['player_id'].map(appearances_df[appearances_columns].groupby('player_id').apply(lambda x: x.to_dict('records')).to_dict())
 
-# Export the merged dataset to JSON
-players_df.to_json('players_merged.json', orient='records', lines=True)
+# Funzione per rimuovere 'player_id' dai record
+def remove_player_id(records):
+    return [{k: v for k, v in record.items() if k != 'player_id'} for record in records]
+
+# Raggruppa per 'player_id' e rimuovi 'player_id' dai record
+players_df['valuations'] = players_df['player_id'].map(player_valuation_df[valuation_columns].groupby('player_id').apply(lambda x: remove_player_id(x.to_dict('records'))).to_dict())
+players_df['appearances'] = players_df['player_id'].map(appearances_df[appearances_columns].groupby('player_id').apply(lambda x: remove_player_id(x.to_dict('records'))).to_dict())
+
+# Esporta il DataFrame unificato in JSON
+players_df.to_json('DB_gen/players_merged.json', orient='records', lines=True)
+
+
