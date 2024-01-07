f2fd7b43b8c8e5f59f01becf833dc9479109e99f
Cristiano <cristianobattistini09@gmail.com>
2024-01-07 14:45:49 +0100

    formatting queries

--- distinct_columns_utils.py
+++ distinct_columns_utils.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 # Load the players dataset
-players_df = pd.read_csv('game_events')
+players_df = pd.read_csv('Old_DB/games')
 
 # Get distinct values in the 'sub_position' column
 #distinct_sub_positions = players_df['sub_position'].unique()
 
 # Get distinct values in the 'position' column
-description = players_df['description'].unique()
+description = players_df['round'].unique()
 
 # Print distinct values
-print("Distinct description:", description)
+print("Distinct round:", description)
